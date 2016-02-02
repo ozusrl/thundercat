b005a7dacad9573f1d50e4225502992134ac5a8d
Baris Aktemur <baris.aktemur@ozyegin.edu.tr>
2016-02-02 15:36:45 +0200

    Scripts folder
    Scripts to install LLVM 3.7.0.

---
+++ scripts/README.md
@@ -0,0 +1,9 @@
+Use the `setup_llvm.sh` file to install a slightly
+modified version of LLVM 3.7.0.
+The modifications are shown in the `llvm_370_spMVlib_patch.diff`
+file in this folder.
+
+The script will download the LLVM code from LLVM's svn repository,
+apply the patch,
+then build the framework.
+Note that this is a time- and resource-consuming process.
---
+++ scripts/setup_llvm.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+PWD=`pwd`
+#REVISION="-r 179000"
+SUFFIX=tags/RELEASE_370/final/
+#SUFFIX=trunk
+DESTINATION=src_370
+BUILD_DIR=build_370
+
+svn co $REVISION http://llvm.org/svn/llvm-project/llvm/$SUFFIX $DESTINATION
+
+cd $DESTINATION/tools
+svn co $REVISION http://llvm.org/svn/llvm-project/cfe/$SUFFIX clang
+cd ../..
+
+cd $DESTINATION/tools/clang/tools
+svn co $REVISION http://llvm.org/svn/llvm-project/clang-tools-extra/$SUFFIX extra
+cd ../../../..
+
+cd $DESTINATION/projects
+svn co $REVISION http://llvm.org/svn/llvm-project/compiler-rt/$SUFFIX compiler-rt
+cd ../..
+
+read -n1 -r -p "SVN checkout complete. Press a key to apply the patch.\n" key
+
+### Apply SpMVlib-specific changes
+cd $DESTINATION
+patch -p0 -i ../llvm_370_spMVlib_patch.diff
+cd ..
+
+read -n1 -r -p "Patch applied. Press a key to start the build process.\n" key
+
+### Compilation
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_CXX1Y=ON ../$DESTINATION
+make -j6
+cd ..
+ln -s $DESTINATION src
+ln -s $BUILD_DIR build
+
+### PATH
+echo "Add the following line to your bash profile."
+echo "export PATH=\$PATH:$PWD/build/bin"
+
