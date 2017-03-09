bb4416f7ef4982b5c18be500e4b7f87f7802691f
Baris Aktemur <baris.aktemur@ozyegin.edu.tr>
2017-03-09 12:58:05 +0300

    Script update for the new matrix folder structure.

--- tools/getMatrixStats.sh
+++ tools/getMatrixStats.sh
@@ -7,8 +7,6 @@ fi
    
 while read matrixName
 do
-    cd ..
     echo -n $matrixName" "
-    ./build/spMVgen $MATRICES/"$matrixName" PlainCSR -matrix_stats -num_threads 8
-    cd - > /dev/null
+    ../build/spMVgen $MATRICES/$matrixName/$matrixName PlainCSR -matrix_stats | tail -n 1
 done < matrixNames.txt
