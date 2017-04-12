49a6646b22ee131361b29395f921057e4175bc8c
Baris Aktemur <baris.aktemur@ozyegin.edu.tr>
2017-04-12 22:32:13 +0300

    Refactored main.

--- src/main.cpp
+++ src/main.cpp
@@ -20,10 +20,37 @@ bool MKL_ENABLED = false;
 bool PLAINCSR_ENABLED = false;
 unsigned int NUM_OF_THREADS = 1;
 int nthreads = 0;
+Matrix *csrMatrix;
+SpMVMethod *method;
+vector<MultByMFun> fptrs;
+double *vVector;
+double *wVector;
+
+
+void parseCommandLineArguments(int argc, const char *argv[]);
+void dumpMatrixIfRequested();
+void doSVMAnalysisIfRequested();
+void setupMKL();
+void generateFunctions();
+void populateInputOutputVectors();
+void benchmark();
+void cleanup();
 
 int main(int argc, const char *argv[]) {
+  parseCommandLineArguments(argc, argv);
+  dumpMatrixIfRequested();
+  doSVMAnalysisIfRequested();
+  setupMKL();
+  generateFunctions();
+  populateInputOutputVectors();
+  benchmark();
+  cleanup();
+  return 0;
+}
+
+void parseCommandLineArguments(int argc, const char *argv[]) {
   // Usage: spMVgen <matrixName> <specializerName> {-debug|-dump_object|-dump_matrix|-num_threads|-matrix_stats}
-  // E.g: spMVgen matrices/fidap037 unfolding 
+  // E.g: spMVgen matrices/fidap037 unfolding
   // E.g: spMVgen matrices/fidap037 unfolding -debug
   // E.g: spMVgen matrices/fidap037 unfolding -dump_object
   // E.g: spMVgen matrices/fidap037 unfolding -num_threads 6
@@ -38,20 +65,19 @@ int main(int argc, const char *argv[]) {
   string csrWithGOTO("CSRWithGOTO");
   string mkl("MKL");
   string plainCSR("PlainCSR");
-
+  
   string debugFlag("-debug");
   string dumpObjFlag("-dump_object");
   string dumpMatrixFlag("-dump_matrix");
   string numThreadsFlag("-num_threads");
   string matrixStatsFlag("-matrix_stats");
-
+  
   string matrixName(argv[1]);
-  Matrix *csrMatrix = Matrix::readMatrixFromFile(matrixName + ".mtx");
+  csrMatrix = Matrix::readMatrixFromFile(matrixName + ".mtx");
   
   char **argptr = (char**)&argv[2];
-
+  
   // Read the specializer
-  SpMVMethod *method = NULL;
   if(genOSKI.compare(*argptr) == 0) {
     int b_r = atoi(*(++argptr));
     int b_c = atoi(*(++argptr));
@@ -83,7 +109,7 @@ int main(int argc, const char *argv[]) {
     exit(1);
   }
   argptr++;
-
+  
   while (argc > argptr - (char**)&argv[0]) { // the optional flag exists
     if (debugFlag.compare(*argptr) == 0)
       __DEBUG__ = true;
@@ -102,23 +128,28 @@ int main(int argc, const char *argv[]) {
     } else {
       std::cout << "Unrecognized flag: " << *argptr << "\n";
       exit(1);
-    } 
+    }
     argptr++;
   }
+}
 
-
+void dumpMatrixIfRequested() {
   if (DUMP_MATRIX) {
     Matrix *matrix = method->getMatrix();
     matrix->print();
     exit(0);
   }
+}
 
+void doSVMAnalysisIfRequested() {
   if (MATRIX_STATS) {
     SVMAnalyzer svmAnalyzer(csrMatrix);
     svmAnalyzer.printFeatures();
     exit(0);
   }
+}
 
+void setupMKL() {
 #ifdef __linux__
   if (!MKL_ENABLED) {
     omp_set_num_threads(NUM_OF_THREADS);
@@ -133,8 +164,9 @@ int main(int argc, const char *argv[]) {
       cout << "Num threads = " << nthreads << "\n";
   }
 #endif
-  
-  vector<MultByMFun> fptrs;
+}
+
+void generateFunctions() {
   START_TIME_PROFILE(codeGeneration);
   if (MKL_ENABLED) {
     MKL *mklMethod = (MKL*)method;
@@ -149,82 +181,92 @@ int main(int argc, const char *argv[]) {
     fptrs = specializer.getMultByMFunctions();
   }
   END_TIME_PROFILE(codeGeneration);
-  
+}
+
+void populateInputOutputVectors() {
   unsigned long n = csrMatrix->n;
   unsigned long nz = csrMatrix->nz;
-  double *v = new double[n];
-  double *w = new double[n];
+  vVector = new double[n];
+  wVector = new double[n];
   for(int i = 0; i < n; ++i) {
-    w[i] = i + 1;
-    v[i] = i + 1;
+    vVector[i] = i + 1;
+    wVector[i] = i + 1;
   }
+}
 
-  unsigned int ITERS;
-  if (nz < 5000) {
-    ITERS = 500000;
-  } else if (nz < 10000) {
-    ITERS = 200000;
-  } else if (nz < 50000) {
-    ITERS = 100000;
-  } else if (nz < 100000) {
-    ITERS = 50000;
-  } else if (nz < 200000) {
-    ITERS = 10000;
-  } else if (nz < 1000000) {
-    ITERS = 5000;
-  } else if (nz < 2000000) {
-    ITERS = 1000;
-  } else if (nz < 3000000) {
-    ITERS = 500;
-  } else if (nz < 5000000) {
-    ITERS = 200;
-  } else if (nz < 8000000) {
-    ITERS = 100;
-  } else if (nz < 12000000) {
-    ITERS = 50;
-  } else {
-    ITERS = 100;
-  }
-  
+unsigned int getNumIterations() {
   if (__DEBUG__) {
-    ITERS = 1;
+    return 1;
   }
 
+  unsigned long nz = csrMatrix->nz;
+
+  if (nz < 5000) {
+    return 500000;
+  } else if (nz < 10000) {
+    return 200000;
+  } else if (nz < 50000) {
+    return 100000;
+  } else if (nz < 100000) {
+    return 50000;
+  } else if (nz < 200000) {
+    return 10000;
+  } else if (nz < 1000000) {
+    return 5000;
+  } else if (nz < 2000000) {
+    return 1000;
+  } else if (nz < 3000000) {
+    return 500;
+  } else if (nz < 5000000) {
+    return 200;
+  } else if (nz < 8000000) {
+    return 100;
+  } else if (nz < 12000000) {
+    return 50;
+  } else {
+    return 100;
+  }
+}
+
+void benchmark() {
+  unsigned int ITERS = getNumIterations();
+  unsigned long n = csrMatrix->n;
+  
   Matrix *matrix = method->getMatrix();
   START_TIME_PROFILE(multByM);
   if (fptrs.size() == 1) {
     for (int i=0; i < ITERS; i++) {
-      fptrs[0](v, w, matrix->rows, matrix->cols, matrix->vals);
+      fptrs[0](vVector, wVector, matrix->rows, matrix->cols, matrix->vals);
     }
-  } else {    
+  } else {
     for (unsigned i=0; i < ITERS; i++) {
-      #pragma omp parallel for
+#pragma omp parallel for
       for (unsigned j = 0; j < fptrs.size(); j++) {
-        fptrs[j](v, w, matrix->rows, matrix->cols, matrix->vals);
+        fptrs[j](vVector, wVector, matrix->rows, matrix->cols, matrix->vals);
       }
     }
   }
   END_TIME_PROFILE(multByM);
-
+  
   if (__DEBUG__) {
-    for(int i = 0; i < n; ++i) 
-      printf("%g\n", w[i]);
+    for(int i = 0; i < n; ++i)
+      printf("%g\n", wVector[i]);
   } else {
     Profiler::print(ITERS);
     std::cout << "0" << std::setw(10) << ITERS << " times    iterated\n";
   }
+}
 
+void cleanup() {
   /* Normally, a cleanup as follows is the client's responsibility.
      Because we're aliasing some pointers in the returned matrices
      for Unfolding, MKL, and PlainCSR, these deallocations would cause
-     double free's. Can be fixed by using shared pointers.   
-  delete csrMatrix;
-  delete matrix;
-  delete[] v;
-  delete[] w;
-  delete method;
+     double free's. Can be fixed by using shared pointers.
+   
+   delete csrMatrix;
+   delete matrix;
+   delete[] v;
+   delete[] w;
+   delete method;
   */
-
-  return 0;
 }
-
